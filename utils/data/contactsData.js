db2744339cec6052f33f6fe4a79f9946adc28e73
Haseeb Usman <haseebusman030576@gmail.com>
2024-09-14 14:13:09 +0500

    feat: Add ESLint configuration file
    Add .eslintrc.json file to configure ESLint with the "next/core-web-vitals" extension.

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'haseebusman.dev@gmail.com',
+    phone: '+923057664298',
+    address: 'Faisalabad, Pakistan',
+    github: 'https://github.com/haseebusman0305',
+    facebook: 'https://www.facebook.com/profile.php?id=100086274824046',
+    linkedIn: 'https://www.linkedin.com/in/haseeb-usman',
+    twitter: 'https://twitter.com/haseeb_usman_',
+    stackOverflow: 'https://stackoverflow.com/users/22800961/haseeb-usman',
+    devUsername: "HaseebUsman"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Haseeb Usman",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is Haseeb Usman. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'haseebusman.dev@gmail.com',
+  phone: '+923057664298',
+  address: 'Faisalabad, Pakistan',
+  github: 'https://github.com/haseebusman0305',
+  facebook: 'https://www.facebook.com/profile.php?id=100086274824046',
+  linkedIn: 'https://www.linkedin.com/in/haseeb-usman',
+  twitter: 'https://twitter.com/haseeb_usman_',
+  stackOverflow: 'https://stackoverflow.com/users/22800961/haseeb-usman',
+  leetcode: "https://leetcode.com/u/haseebusman030576/",
+  devUsername: "HaseebUsman",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
